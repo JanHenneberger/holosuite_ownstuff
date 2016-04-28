d49c7574860f9414def06f0cf91672b86df30b85
Jan Henneberger <Jan.Henneberger@env.ethz.ch>
2016-04-28 09:37:51 +0200

    Add Glaciation Time

--- plot_ScatterUpdraftOlga3.m
+++ plot_ScatterUpdraftOlga3.m
@@ -14,7 +14,7 @@ function plot_ScatterUpdraftOlga2(anData,cntFig)
     clf
 
     anData.chosenData
-    s1 = subplot(1,2,1);
+    s1 = subplot(1,3,1);
     
     g1 = gscatter(anData.meteoTemperature(anData.chosenData), anData.uz(anData.chosenData)...
         ,group(anData.chosenData), ...
@@ -23,13 +23,13 @@ function plot_ScatterUpdraftOlga2(anData,cntFig)
     xlabel('Temperature [°C]')
     xlim([-30 0])
     ylabel('Required vertical velocity [m s^{-1}]')
-    set(gca, 'YLim',[0.003 70],'yscale', 'log',...
+    set(gca, 'YLim',[0.001 10],'yscale', 'log',...
         'YTickLabel',{'.001';'0.01';'0.1';'1';'10';'100';'1000';'10000'},'Ytick',[0.001 0.01 0.1 1 10 100 1000 10000]);
     box on
     
     
       
-    s2 = subplot(1,2,2);
+    s2 = subplot(1,3,2);
     
     g2 = gscatter(anData.IWConcentraction(anData.chosenData), anData.uz(anData.chosenData)...
         ,group(anData.chosenData), ...
@@ -37,14 +37,28 @@ function plot_ScatterUpdraftOlga2(anData,cntFig)
     xlabel('Ice concentration [cm^{-3}]')
     xlim([0.0015 12])
     set(gca, 'xscale','log')
-    set(gca, 'YLim',[0.003 70],'yscale', 'log',...
-        'YTickLabel',{'.001';'0.01';'0.1';'1';'10';'100';'1000';'10000'},'Ytick',[0.001 0.01 0.1 1 10 100 1000 10000]);
-    set(gca, 'XTickLabel',{'0.001';'0.01';'0.1';'1';},'XTick',[0.001 0.01 0.1 1])
+    set(gca, 'YLim',[0.001 10],'yscale', 'log',...
+        'YTickLabel',{'.001';'0.01';'0.1';'1';'10';'100';'1000';'10000'},...
+        'Ytick',[0.001 0.01 0.1 1 10 100 1000 10000]);
      l = findobj(gcf,'tag','legend'); 
 %     l.Position(1) = 0.57;
      set(l,'location','northwest','fontsize',9);
     box on
-        
+     
+        s2 = subplot(1,3,3);
+    
+    g2 = gscatter(anData.glaciationTime(anData.chosenData), anData.uz(anData.chosenData)...
+        ,group(anData.chosenData), ...
+        color,symbol ,8,'off');
+    xlabel('Glaciation time [s]')
+    set(gca, 'XLim',[0.1 10000],'Xscale', 'log',...
+        'XTickLabel',{'.001';'0.01';'0.1';'1';'10';'100';'1000';'10000'},...
+        'Xtick',[0.001 0.01 0.1 1 10 100 1000 10000]);
+    set(gca, 'YLim',[0.001 10],'yscale', 'log',...
+        'YTickLabel',{'.001';'0.01';'0.1';'1';'10';'100';'1000';'10000'},...
+        'Ytick',[0.001 0.01 0.1 1 10 100 1000 10000]);
+    box on
+    
 %        s3 = subplot(1,3,3);
 %     
 %     g3 = gscatter(anData.IWMeanD(anData.chosenData)*1e6, anData.uz(anData.chosenData)...
@@ -62,8 +76,8 @@ function plot_ScatterUpdraftOlga2(anData,cntFig)
         mtit([anData.campaignName])
     end
     set(gcf, 'PaperUnits','centimeters');
-    set(gcf, 'PaperPosition',[-1 .2 24 9]);
-    set(gcf, 'PaperSize', [22 9]);
+    set(gcf, 'PaperPosition',[-1 .2 34 9]);
+    set(gcf, 'PaperSize', [32 9]);
     
     if anData.savePlots
         fileName = ['Scatter_UpdraftVelOlga3'];
