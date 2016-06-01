ebfc50c7f911d576a768ea742c062cfd435d06af
Jan Henneberger <Jan.Henneberger@env.ethz.ch>
2016-06-01 17:04:20 +0200

    Use it for 2015 and 2016

--- GUISatelite.m
+++ GUISatelite.m
@@ -55,47 +55,74 @@ function GUISatelite_OpeningFcn(hObject, eventdata, handles, varargin)
 % Choose default command line output for GUISatelite
 handles.output = hObject;
 
-if numel(varargin) > 0
-    handles.meteoPrecip = varargin{1};
-else
+if numel(varargin) ==  0
+    handles.campaign = '2013';
+else    
+    handles.campaign = varargin{1};
+end
+if strcmp(handles.campaign,'2013')
     handles.meteoPrecip = wolke.importMeteoData('Z:\3_Data\CLACE2013\MeteoSwissPrecip\order_40015_data.txt');
-end
-
-if numel(varargin) > 1
-    handles.meteoJFJ = varargin{2};
-else
     handles.meteoJFJ = wolke.importMeteoData('Z:\3_Data\CLACE2013\MeteoSwissJFJ\order_40175_data.txt');
-end
-
-if numel(varargin) > 2
-    handles.holimo = varargin{3};
-else
     handles.holimo = load('Z:\6_Auswertung\ALL\Neusten\HOLIMO_100sec.mat');
-    handles.holimo = handles.holimo.outFile;
+    handles.holimo = handles.holimo.outFile;   
+    %create UIFigure for HOLIMO Data
+    handles.fHOLIMO = figure('Position',[2000,0, 1600,1000],'NumberTitle','off','MenuBar','none');
+
+    handles.saveScreenpath = 'Z:\6_Auswertung\ALL\RadarSatObsPlot';
+    handles.basepath = 'Z:\3_Data\CLACE2013\JFJ_Remote';
+    handles.DWDpath = 'DWD_charts';
+    
+    handles.year = 2013;
+    handles.month = 02;
+    handles.day = 01;
+    
+    handles.stepRadar = 30;
+    handles.SatNr = 24;
+    handles.stepSat = 1;
+    handles.stepAll = 2;
+elseif strcmp(handles.campaign,'2015')
+    
+    handles.meteoJFJ = wolke.importMeteoData('Z:\3_Data\2015\MeteoSchweiz\order_34867_data.txt');
+    
+    handles.saveScreenpath = 'Z:\6_Auswertung\JFJ2015\RadarSatObsPlot';
+    handles.basepath = 'Z:\3_Data\2015';
+    handles.DWDpath = 'NOAA';
+     
+
+    handles.year = 2015;
+    handles.month = 02;
+    handles.day = 01;
+    
+    handles.stepRadar = 28;
+    handles.SatNr = 24;
+    handles.stepSat = 1;
+    handles.stepAll = 2;
+    
+elseif strcmp(handles.campaign,'2016')
+    handles.meteoJFJ = wolke.importMeteoData('Z:\3_Data\2016\MeteoSchweiz\order_43172_data.txt');
+    handles.meteoEGH = wolke.importMeteoData('Z:\3_Data\2016\MeteoSchweiz\order_41800_data.txt');
+    handles.saveScreenpath = 'Z:\6_Auswertung\JFJ2016\RadarSatObsPlot';
+    handles.basepath = 'Z:\3_Data\2016';
+    handles.DWDpath = 'dwd_charts';
+     
+
+    handles.year = 2016;
+    handles.month = 03;
+    handles.day = 01; 
+    
+    handles.stepRadar = 28;
+    handles.SatNr = 48;
+    handles.stepSat = 2;
+    handles.stepAll = 2;
 end
 
-handles.saveScreenpath = 'Z:\6_Auswertung\ALL\RadarSatObsPlot';
-handles.basepath = 'Z:\3_Data\CLACE2013\JFJ_Remote';
-handles.DWDpath = 'DWD_charts';
 handles.Radarpath = 'Radar';
 handles.SatIRpath = 'Satellite\IR';
 handles.SatVISpath = 'Satellite\VIS';
-
-handles.year = 2013;
-handles.month = 02;
-handles.day = 01;
+    
 handles.hour = 12;
 handles.min = 00;
 handles.sec = 00;
-handles.stepRadar = 30;
-
-handles.SatNr = 24;
-handles.stepSat = 1;
-
-handles.stepAll = 2;
-
-%create UIFigure for HOLIMO Data
-handles.fHOLIMO = figure('Position',[2000,0, 1600,1000],'NumberTitle','off','MenuBar','none');
 
 handles = refreshAll(handles);
 
@@ -121,7 +148,9 @@ function figure1_CloseRequestFcn(hObject, eventdata, handles)
 % hObject    handle to figure1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+try
 close(handles.fHOLIMO)
+end
 % Hint: delete(hObject) closes the figure
 delete(hObject);
 
@@ -415,7 +444,7 @@ handles = updatedate(handles);
 plotRadar(handles);
 plotSat(handles);
 plotRest(handles);
-plotHolimo(handles);
+%plotHolimo(handles);
 
 function handles = refreshRadar(handles)
 handles = updatedate(handles);
@@ -461,7 +490,7 @@ try
     cla(ax)
     path = fullfile(handles.basepath, handles.Radarpath, handles.datepath);
     
-    if dateVector(1) == 2013
+    if dateVector(1) >= 2013
         name = [datestr(handles.datenum, 'yyyymmddHHMM') '.gif'];
     else
         name = ['TGN' datestr(handles.datenum, 'yyyymmddHHMM') '.gif'];
@@ -470,7 +499,7 @@ try
     colormap(ax, map1);
     imshow(img, map1,'Parent',ax)
     hold(ax,'on')
-    if dateVector(1) == 2013
+    if dateVector(1) >= 2013
         plot(388,401,'rx','MarkerSize',20,'Parent',ax);
     else
         plot(170,162,'rx','MarkerSize',20,'Parent',ax);
@@ -510,7 +539,7 @@ try
     colormap(ax, map3);
     imshow(img,'Parent',ax)
     hold(ax,'on')    
-%     if dateVector(1) == 2013
+%     if dateVector(1) >= 2013
 %         plot(407,361,'rx','MarkerSize',20,'Parent',ax);
 %     else
 %         plot(484,361,'rx','MarkerSize',20,'Parent',ax);
@@ -524,18 +553,26 @@ end
 
 function plotRest(handles)
 dateVector =datevec(handles.datenum);
+
 try
     ax = handles.axesDWDchart;
     cla(ax)
     path = fullfile(handles.basepath, handles.DWDpath, handles.datepath, 'analysis');
-    name = ['bwk_m_stat_' datestr(handles.datenum, 'yymmdd') '0600.tif'];
-    [img map] = imread(fullfile(path,name));
-    img = imrotate(img, 90);
+    if strcmp(handles.campaign,'2016')
+        name = ['bwk_m_stat_' datestr(handles.datenum, 'yymmdd') '0600.png'];
+        [img map] = imread(fullfile(path,name));
+    else
+        name = ['bwk_m_stat_' datestr(handles.datenum, 'yymmdd') '0600.tif'];
+        [img map] = imread(fullfile(path,name));
+        img = imrotate(img, 90);
+    end
     colormap(ax, [0 0 0 ; 1 1 1]);
     %img = imcomplement(img);
     imshow(img,'Parent',ax)
     hold(ax,'on')
-    if dateVector(1) == 2013
+    if dateVector(1) >= 2016
+        plot(11218,4716,'rx','MarkerSize',20,'Parent',ax);
+    elseif dateVector(1) >= 2013
         plot(1900,1400,'rx','MarkerSize',20,'Parent',ax);
     else
         plot(1900,1580,'rx','MarkerSize',20,'Parent',ax);
@@ -546,6 +583,8 @@ catch
     warning([path, 'does not exists'])
 end
 
+    
+if strcmp(handles.campaign,'2013')
 try
     ax = handles.axesMeteoPrecip;
     cla(ax)
@@ -570,6 +609,63 @@ catch
     text(0.5, 0.5, 'not available','Parent',ax)
     warning([path, 'does not exists'])
 end
+elseif strcmp(handles.campaign,'2015')
+    try
+        ax = handles.axesMeteoPrecip;
+        cla(ax)
+        starttime = datenum([handles.year handles.month handles.day]);
+        endtime = starttime + 1;
+        
+        datax = handles.meteoJFJ.EGH.datenum;
+        idx = datax > starttime & datax < endtime;
+        
+        
+        datay1 = handles.meteoJFJ.EGH.Temperature;
+        
+        datay3 = handles.meteoJFJ.EGH.Wind_speed;
+        
+        
+        [a, h1, h2] = plotyy(datax(idx),datay1(idx),...
+            datax(idx), datay3(idx),...
+            'Parent',ax);
+        
+        ylabel(a(1),'Temperature [°C]') % left y-axis
+        ylabel(a(2),'Wind speed [m s^{-1}]') % right y-axis
+        legend(ax,{'Temperature';'Wind speed'})
+        datetick(ax,'x','HH-MM','keeplimits')
+    catch
+        text(0.5, 0.5, 'not available','Parent',ax)
+        warning([path, 'does not exists'])
+    end
+elseif strcmp(handles.campaign,'2016')
+    try
+        ax = handles.axesMeteoPrecip;
+        cla(ax)
+        starttime = datenum([handles.year handles.month handles.day]);
+        endtime = starttime + 1;
+        
+        datax = handles.meteoEGH.EGH.datenum;
+        idx = datax > starttime & datax < endtime;
+        
+        
+        datay1 = handles.meteoEGH.EGH.Temperature;
+        
+        datay3 = handles.meteoEGH.EGH.Wind_speed;
+        
+        
+        [a, h1, h2] = plotyy(datax(idx),datay1(idx),...
+            datax(idx), datay3(idx),...
+            'Parent',ax);
+        
+        ylabel(a(1),'Temperature [°C]') % left y-axis
+        ylabel(a(2),'Wind speed [m s^{-1}]') % right y-axis
+        legend(ax,{'Temperature';'Wind speed'})
+        datetick(ax,'x','HH-MM','keeplimits')
+    catch
+        text(0.5, 0.5, 'not available','Parent',ax)
+        warning([path, 'does not exists'])
+    end
+end
 
 try
     ax = handles.axesMeteoJFJ;
@@ -578,20 +674,18 @@ try
     endtime = starttime + 1;
     
     datax = handles.meteoJFJ.JUN.datenum;
-    idx = datax > starttime & datax < endtime;
-    
+    idx = datax > starttime & datax < endtime;   
   
     
-    datay1 = handles.meteoJFJ.JUN.Temperature;
-    
+    datay1 = handles.meteoJFJ.JUN.Temperature;   
     
     datay3 = handles.meteoJFJ.JUN.Wind_speed;
     
     
     [a, h1, h2] = plotyy(datax(idx),datay1(idx),...
-           datax(idx), datay3(idx),...
-           'Parent',ax);
-
+        datax(idx), datay3(idx),...
+        'Parent',ax);
+    
     ylabel(a(1),'Temperature [°C]') % left y-axis
     ylabel(a(2),'Wind speed [m s^{-1}]') % right y-axis
     legend(ax,{'Temperature';'Wind speed'})
@@ -601,33 +695,59 @@ catch
     warning([path, 'does not exists'])
 end
 
-try
-    ax = handles.axesMeteoJFJ2;
-    cla(ax)
-    starttime = datenum([handles.year handles.month handles.day]);
-    endtime = starttime + 1;
-    
-    datax = handles.meteoJFJ.JUN.datenum;
-    idx = datax > starttime & datax < endtime;
-    
+if strcmp(handles.campaign,'2013') || strcmp(handles.campaign,'2016')
+    try
+        ax = handles.axesMeteoJFJ2;
+        cla(ax)
+        starttime = datenum([handles.year handles.month handles.day]);
+        endtime = starttime + 1;
+        
+        datax = handles.meteoJFJ.JUN.datenum;
+        idx = datax > starttime & datax < endtime;
+        
+        
+        
+        datay1 = handles.meteoJFJ.JUN.Relative_humidity;
+        
+        datay3 = handles.meteoJFJ.JUN.GlobalRadiation/nanmax(handles.meteoJFJ.JUN.GlobalRadiation);
+        datay4 = handles.meteoJFJ.JUN.SunshineDuration/nanmax(handles.meteoJFJ.JUN.SunshineDuration);
+        
+        [a, h1, h2] = plotyy(datax(idx),datay1(idx),...
+            [datax(idx)', datax(idx)'],[datay3(idx)',datay4(idx)'],...
+            'Parent',ax);
+        ylabel(a(1),'relative humidity [%]') % left y-axis
+        ylabel(a(2),'radiation and sunshine [%]') % right y-axis
+        
+        legend(ax,{'rel. Humidity';'global Radiation';'Sunshine Duration'})
+        datetick(ax,'x','HH-MM','keeplimits')
+    catch
+        text(0.5, 0.5, 'not available','Parent',ax)
+        warning([path, 'does not exists'])
+    end
+elseif strcmp(handles.campaign,'2015')
+    try
+        ax = handles.axesMeteoJFJ2;
+        cla(ax)
+        starttime = datenum([handles.year handles.month handles.day]);
+        endtime = starttime + 1;
+        
+        datax = handles.meteoJFJ.JUN.datenum;
+        idx = datax > starttime & datax < endtime;
+        
+        
+        
+        datay1 = handles.meteoJFJ.JUN.Relative_humidity;
+        datay2 = handles.meteoJFJ.EGH.Relative_humidity;
 
-    
-    datay1 = handles.meteoJFJ.JUN.Relative_humidity;  
-    
-    datay3 = handles.meteoJFJ.JUN.GlobalRadiation/nanmax(handles.meteoJFJ.JUN.GlobalRadiation);
-    datay4 = handles.meteoJFJ.JUN.SunshineDuration/nanmax(handles.meteoJFJ.JUN.SunshineDuration);
-    
-    [a, h1, h2] = plotyy(datax(idx),datay1(idx),...
-           [datax(idx)', datax(idx)'],[datay3(idx)',datay4(idx)'],...
-           'Parent',ax);
-    ylabel(a(1),'relative humidity [%]') % left y-axis
-    ylabel(a(2),'radiation and sunshine [%]') % right y-axis
+        plot(datax(idx),datay1(idx),datax(idx),datay2(idx),'Parent',ax);
    
-    legend(ax,{'rel. Humidity';'global Radiation';'Sunshine Duration'})
-    datetick(ax,'x','HH-MM','keeplimits')
-catch
-    text(0.5, 0.5, 'not available','Parent',ax)
-    warning([path, 'does not exists'])
+        ylabel(ax, 'relative humidity [%]') % left y-axis
+        legend(ax, 'JFJ', 'EGH')
+        datetick(ax,'x','HH-MM','keeplimits')
+    catch
+        text(0.5, 0.5, 'not available','Parent',ax)
+        warning([path, 'does not exists'])
+    end
 end
 
 function plotHolimo(handles)
